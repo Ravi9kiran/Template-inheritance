7e3b558cdb48d57eee85a547ed49c902050c616c
Ravi9kiran <varikutiravikiran2001@gmail.com>
2023-01-19 21:13:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Ravi(request):
+    return render(request,'Ravi.html')
+
+def Kiran(request):
+    return render(request,'Kiran.html')
